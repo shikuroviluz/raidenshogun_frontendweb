c897ab5dab34e8c6c57816b4cb43f3568083adb6
shikuro viluz <ahmadvirza89@gmail.com>
2026-02-13 15:59:26 +0700

    fix asset and font path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/', // ini sudah benar untuk Netlify
   plugins: [
-    react() ,tailwindcss()
+    react(),
+    tailwindcss()
   ],
+  build: {
+    outDir: 'dist'
+  }
 })
